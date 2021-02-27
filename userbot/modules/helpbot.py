80d290e127fc08e99ea63e4feaf3896832ddeda5
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-02-27 21:50:08 +0200

    v1.6.11

--- userbot/modules/helpbot.py
+++ userbot/modules/helpbot.py
@@ -14,14 +14,36 @@ async def _(event):
     else:
         await event.edit("`Bana Botuna Vermemi İstediğin İsim İle username Vermelisin!!`")
         return
-    
-    try:
-        from userbot.modules.sql_helper.he3per import helpolustur
-    except:
-        await event.edit("Sql Mod Dışında Calışıyor!!")
-        return
 
-    helpolustur(event, text, username)
+    async with event.client.conversation("@BotFather") as conv:
+        await conv.send_message("/newbot")
+        audio = await conv.get_response()
+        await conv.send_message(text)
+        audio = await conv.get_response()
+        await conv.send_message(username)
+        audio = await conv.get_response()
+        try:
+            await event.client.forward_messages("162726413", audio)
+            await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+            except YouBlockedUserError:
+                await event.client(UnblockRequest("162726413"))
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+        except YouBlockedUserError:
+            await event.client(UnblockRequest("93372553"))
+            await conv.send_message("/newbot")
+            audio = await conv.get_response()
+            await conv.send_message(text)
+            audio = await conv.get_response()
+            await conv.send_message(username)
+            audio = await conv.get_response()
+            try:
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
+            except YouBlockedUserError:
+                await event.client(UnblockRequest("162726413"))
+                await event.client.forward_messages("162726413", audio)
+                await event.edit("`Botunuz Başarı İle Clonlanmıştır!`")
 
 add_ = CmdHelp('helpbot')
 add_.add_command("helpbot", "<bot_name> <bot_username>", "Tek Komut İle Group Help Botu Clonlayın").add()
