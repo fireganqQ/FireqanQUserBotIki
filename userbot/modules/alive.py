dc295bbb97994d7c5e75042e5c516516dece975a
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-03-12 20:25:34 +0200

    v1.10.12

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -9,7 +9,7 @@ from userbot.events import register
 from telethon import events
 from userbot.main import PLUGIN_MESAJLAR
 
-ALİVE_ = "True"
+ALİVE_ = True
 ALİVE_N_ = None
 ALİVE_I_ = None
 ALİVE_P_ = None
@@ -22,7 +22,7 @@ def degiskenler():
 	global DEFAULTUSER
 	global id_
 	global ALIVE_PIC
-	if ALİVE_=="True":
+	if ALİVE_:
 		if ALİVE_N_:
 			DEFAULTUSER = ALİVE_N_
 			if ALİVE_I_:
@@ -114,51 +114,48 @@ async def _(q):
 
 	text = q.pattern_match.group().split()
 	text.pop(0)
-	try:
-		text, text_2=text
-		if text == "" or text == " ":
-			await q.edit("`Gecersiz Değer Girdiniz Değiştire Bileceğiniz Değerler =>` `alive``/``name``/``pic``/``id`")
-			return
-	except:
+
+	text, text_2=text
+
+	if text.lower() in a_:
+		if text.lower() == "alive":
+			if text_2.lower() != "true" and text_2.lower() != "false" or text_2.lower() == "":
+				await q.edit("`Sadece True Veya False Giriniz...`")
+				return
+			else:
+				if text_2.lower() == "true":
+					ALİVE_ = True
+					await q.edit("`Alive Değeriniz True Olarak Değiştirilmiştir...`")
+					return
+	
+				if text_2.lower() == "false":
+					ALİVE_ = False
+					await q.edit("`Alive Değeriniz Fasle Olarak Değiştirilmiştir...`")
+					return
+		if text.lower() == "name":
+			if text_2.lower() != "":
+				ALİVE_N_=text_2
+				await q.edit("`Name Değeriniz {} Olarak Değiştirilmiştir...`".format(text_2))
+				return
+			else:
+				await q.edit("`Name Değişkenini Değiştirmem İçin Bana Bir İsim Vermelisim!!`")
+				return
+	
+		if text.lower() == "pic":
+			if text_2.lower() != "":
+				ALİVE_P_=text_2
+				await q.edit("`Alive Pic Linkiniz {} Olarak Değiştirilmiştir...`".format(text_2))
+				return
+			else:
+				await q.edit("`Pic Değişkenini Değiştirmem İçin Bana Bir Link Vermelisim!!`")
+				return
+
+		if text.lower() == "id":
+			if text_2.lower() != "":
+				ALİVE_I_=text_2
+				await q.edit("`İd Değişkeniniz {} Olarak Değiştirilmiştir...`".format(text_2))
+				return
+			else:
+				await q.edit("`İd Değişkenini Değiştirmem İçin Bana Bir İd Vermelisim!!`")
+	else:
 		await q.edit("`Gecersiz Değer Girdiniz Değiştire Bileceğiniz Değerler =>` `alive``/``name``/``pic``/``id`")
-		return
-
-	if text.lower() == "alive":
-		if text_2.lower() != "true" and text_2.lower() != "false" or text_2.lower() == "":
-			await q.edit("`Sadece True Veya False Giriniz...`")
-			return
-		else:
-			if text_2.lower() == "true":
-				ALİVE_ = "True"
-				await q.edit("`Alive Değeriniz True Olarak Değiştirilmiştir...`")
-				return
-
-			if text_2.lower() == "false":
-				ALİVE_ = "False"
-				await q.edit("`Alive Değeriniz Fasle Olarak Değiştirilmiştir...`")
-				return
-	if text.lower() == "name":
-		if text_2.lower() != "":
-			ALİVE_N_=text_2
-			await q.edit("`Name Değeriniz {} Olarak Değiştirilmiştir...`".format(text_2))
-			return
-		else:
-			await q.edit("`Name Değişkenini Değiştirmem İçin Bana Bir İsim Vermelisim!!`")
-			return
-
-	if text.lower() == "pic":
-		if text_2.lower() != "":
-			ALİVE_P_=text_2
-			await q.edit("`Alive Pic Linkiniz {} Olarak Değiştirilmiştir...`".format(text_2))
-			return
-		else:
-			await q.edit("`Pic Değişkenini Değiştirmem İçin Bana Bir Link Vermelisim!!`")
-			return
-
-	if text.lower() == "id":
-		if text_2.lower() != "":
-			ALİVE_I_=text_2
-			await q.edit("`İd Değişkeniniz {} Olarak Değiştirilmiştir...`".format(text_2))
-			return
-		else:
-			await q.edit("`İd Değişkenini Değiştirmem İçin Bana Bir İd Vermelisim!!`")
