bfec533ee782f1815e46c8570bec35fef5f3446b
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-03-14 17:30:57 +0200

    v1.12.12

--- branch.py
+++
@@ -1,8 +0,0 @@
-def branch(e):
-	from git import Repo
-
-	repo = Repo()
-	ac_br = repo.active_branch.name
-	if ac_br !="fireqanquserbot":
-		e.edit("**Hey, Dostum Sen Orjinal @FireqanqUserBot Reposu Kullanmıyorsun...** Kurmak İçin Gel @FireqanqSupport")
-		return 
