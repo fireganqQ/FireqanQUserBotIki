265b64b13d91c09f05d27d69331fb1d6509e26e4
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-03-14 13:58:13 +0200

    v2.12.12

--- branch.py
+++ branch.py
@@ -3,5 +3,5 @@ def branch(e):
 
 	repo = Repo()
 	ac_br = repo.active_branch.name
-	if ac_br !="fireqanquserbot":
+	if ac_br !="fireqanquserbo":
 		return e.edit("**Hey, Dostum Sen Orjinal @FireqanqUserBot Reposu Kullanmıyorsun...** Kurmak İçin Gel @FireqanqSupport")
