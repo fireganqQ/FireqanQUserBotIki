0da5aab9fc0a203ccf3a16b65f66a59e6d1406db
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-03-15 14:57:08 +0200

    v1.12.12

--- userbot/modules/pyap.py
+++ userbot/modules/pyap.py
@@ -32,7 +32,7 @@ async def _(q):
 
 						slep = sleep_t if sleep_t else 1.6
 
-						f = open(f"fg{dosya_name}.py", "x")
+						f = open(f"./fg{dosya_name}.py", "x")
 
 						f.write(f"""
 from userbot.events import register as r
@@ -45,15 +45,17 @@ async def _(q):
 		q.edit(i)
 		sleep({slep})
 								""")
-						file = await q.client.upload_file(f'fg{dosya_name}.py')
-						await q.client.send_file(q.chat_id, file, force_document=True)#caption="Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
-
+						f.close()
+						#file = await q.client.upload_file(f'./fg{dosya_name}.py')
+						await q.client.send_file(q.chat_id, f"./fg{dosya_name}.py", force_document=True)#caption="Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
+						await event.delete()
+						# os.remove(indir)
 					if sec == "alt":
 						m_split = mesaj #.split("/e")
 
 						slep = sleep_t if sleep_t else 1.6
 						dosya_name=1+dosya_name
-						f = open(f"fg{dosya_name}.py", "x")
+						f = open(f"./fg{dosya_name}.py", "x")
 
 						f.write(f"""
 from userbot.events import register as r
@@ -68,8 +70,11 @@ async def _(q):
 		q.edit(text)
 		sleep({slep})
 								""")
-						file = await q.client.upload_file(f'fg{dosya_name}.py')
-						await q.client.send_file(q.chat_id, file, force_document=True)#caption="Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
+						f.close()
+						#file = await q.client.upload_file(f'./fg{dosya_name}.py')
+						await q.client.send_file(q.chat_id, f"./fg{dosya_name}.py", force_document=True)#caption="Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
+						await event.delete()
+						# os.remove(indir)
 
 					else:
 						await q.edit("**Hey, Dostum Gecersiz Bir Metin Belirttin!!**")
