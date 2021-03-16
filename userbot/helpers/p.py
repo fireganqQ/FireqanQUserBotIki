b3a8a083d736e2c4722a99b28cac22db61652682
fireganqQ <66991973+fireganqQ@users.noreply.github.com>
2021-03-16 13:55:35 +0300

    v1.12.12

---
+++ userbot/helpers/p.py
@@ -0,0 +1,43 @@
+def e_(dosya_name, name, slep, liste):
+	f = open(f"./fguserbot{dosya_name}.py", "x")
+	f.write(f"""from userbot.events import register as r
+from userbot.cmdhelp import CmdHelp as c
+from time import sleep as s
+from telethon import events
+
+a={liste}
+
+@r(outgoing=True, pattern="^.{name}$")
+async def _(q):
+	for i in a:
+		await q.edit(' '+str(i))
+		s({slep})
+
+c_ = c("fguserbot{dosya_name}")
+c_.add_command("{name}", None, "Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
+c_.add()
+								""")
+	return f.close()
+
+def a_(dosya_name, name, liste, slep):
+	f = open(f"./fguserbot{dosya_name}.py", "x")
+	f.write(f"""from userbot.events import register as r
+from userbot.cmdhelp import CmdHelp as c
+from time import sleep as s
+from telethon import events
+
+a={liste}
+
+@r(outgoing=True, pattern="^.{name}$")
+async def _(q):
+	text= " "
+	for i in a:
+		text+=i+"\\n"
+		await q.edit(text)
+		s({slep})
+
+c_ = c("fguserbot{dosya_name}")
+c_.add_command("{name}", None, "Bu Plugin @FireqanqUserBot Taradından Yapılmıştır..")
+c_.add()
+								""")
+	return f.close()
